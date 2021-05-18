9731e3e17e8f1a7480559f98c2941586a31ea680
cspartho <cspartho19@gmail.com>
2021-05-18 12:20:56 +0600

    add  models

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Products,Auction
 # Register your models here.
+
+admin.site.register(Products)
+admin.site.register(Auction)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Products(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=500)
+    date_posted = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return "ID:" + str(self.pk) + " " + self.title
+
+class Auction(models.Model):
+	product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
+	number_of_bids = models.IntegerField()
+	time_starting = models.DateTimeField()
+	time_ending = models.DateTimeField()
+	
+	def __str__(self):
+		return "ID:" + str(self.pk) + " PRODUCT_ID:" + str(self.product_id)
